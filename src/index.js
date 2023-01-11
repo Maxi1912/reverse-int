2f25cfdb290b530653ec8510cab269ab94e934fe
Maksym <maximus.sheva@gmail.com>
2023-01-11 12:16:53 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const reverseArr = n.toString().split("").reverse();
+    return parseInt(reverseArr.join(""));
+};
